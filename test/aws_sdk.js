23e4de5568cdf1900566bd9d41067f30a8450f56
Adron Hall <adronhall@gmail.com>
2014-10-19 16:22:25 -0700

    Added the SDK configuration being set.

--- test/aws_sdk.js
+++ test/aws_sdk.js
@@ -1,32 +1,39 @@
 /**
  * Created by adron on 9/24/14.
- * Description: Test to verify the SDK is installed and the 
- *	  security information is available for the SDK to use.
+ * Description: Test to verify the SDK is installed and the
+ *    security information is available for the SDK to use.
  */
 
-var should = require ('should');
-var AWS = require('aws-sdk'); 
+var should = require('should');
+var AWS = require('aws-sdk');
+var awsRegion = 'us-west-2';
 
-describe ( 'When trying out this sample application in AWS you', function () {
+describe('When trying out this sample application in AWS you', function () {
+  "use strict";
 
-  it ( 'should have an environment variable set for AWS_ACCESS_KEY_ID', function () {
-  	process.env.AWS_ACCESS_KEY_ID.should.exist;
+  before(function () {
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: awsRegion});
+  })
+
+  it('should have an environment variable set for AWS_ACCESS_KEY_ID', function () {
+    process.env.AWS_ACCESS_KEY_ID.should.exist;
   });
 
-  it ( 'should have an environment variables set for AWS_SECRET_KEY', function () {
-  	process.env.AWS_SECRET_KEY.should.exist;
+  it('should have an environment variables set for AWS_SECRET_KEY', function () {
+    process.env.AWS_SECRET_KEY.should.exist;
   })
 
-  it ( 'should have the AWS SDK installed and constructable', function () {
-  	AWS.should.exist;
+  it('should have the AWS Access Key set in the AWS config', function () {
+    var config = AWS.config;
+    config.credentials.accessKeyId.should.equal(process.env.AWS_ACCESS_KEY_ID);
+    config.credentials.secretAccessKey.should.equal(process.env.AWS_SECRET_KEY);
   })
 
-  it ( 'should have the AWS Access Key set in the AWS config', function () {
-  	AWS.config.should.exist;
-  })
-
-  it ( 'should have be able to create an S3 bucket', function () {
-  	var s3 = new AWS.S3();
+  it('should have the AWS region set to us west 2', function () {
+    AWS.config.region.should.equal(awsRegion);
   })
 
 });
