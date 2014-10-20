8e6156fe9061e0f0e2d87527c909ad5fb0d8c355
Adron Hall <adronhall@gmail.com>
2014-10-19 17:49:11 -0700

    Got a first stab at a running app.

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
+var AWS = require('aws-sdk')
+
 var Hapi = require('hapi');
+var Good = require('good');
+
 var server = new Hapi.Server(3000);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
+server.pack.register(Good, function (err) {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
