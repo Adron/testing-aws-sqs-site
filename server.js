87e82ae3b70841d21711eed59b09521cead749e5
Adron Hall <adronhall@gmail.com>
2014-10-29 15:42:37 -0700

    Added some send message code.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var AWS = require('aws-sdk')
-
 var Hapi = require('hapi');
 var Good = require('good');
 
@@ -13,10 +12,31 @@ server.route({
   }
 });
 
+function sendSqsMessage() {
+  "use strict";
+
+  var params = {
+    MessageBody: "The Message Body Goes Here",
+    QueueUrl: "https://sqs.us-west-2.amazonaws.com/588271471917/sample",
+    DelaySeconds: 0
+  };
+
+  sqs.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } // an error occurred
+    else {
+      console.log('Victory, message sent for ' + encodeURIComponent(request.params.name) + '!');
+    }
+    ;
+  });
+}
+
 server.route({
   method: 'GET',
   path: '/{name}',
   handler: function (request, reply) {
+    sendSqsMessage(encodeURIComponent(request.params.name));
     reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
   }
 });
--- test/aws_sdk.js
+++ test/aws_sdk.js
@@ -4,62 +4,81 @@
  *    security information is available for the SDK to use.
  */
 
-var should = require('should'),
-  AWS = require('aws-sdk'),
-  awsRegion = 'us-west-2',
+var should = require("should"),
+  AWS = require("aws-sdk"),
+  awsRegion = "us-west-2",
   sqs = {};
 
-describe('When trying out this sample application in AWS you', function () {
+describe("When trying out this sample application in AWS you", function () {
   "use strict";
 
   before(function () {
     AWS.config.update({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_KEY,
-      region: awsRegion});
+      region: awsRegion
+    });
     sqs = new AWS.SQS();
   })
 
-  it('should have an environment variable set for AWS_ACCESS_KEY_ID', function () {
+  it("should have an environment variable set for AWS_ACCESS_KEY_ID", function () {
     console.log(process.env.AWS_ACCESS_KEY_ID);
     process.env.AWS_ACCESS_KEY_ID.should.exist;
   });
 
-  it('should have an environment variables set for AWS_SECRET_KEY', function () {
+  it("should have an environment variables set for AWS_SECRET_KEY", function () {
     console.log(process.env.AWS_SECRET_KEY);
     process.env.AWS_SECRET_KEY.should.exist;
   })
 
-  it('should have the AWS Access Key set in the AWS config', function () {
+  it("should have the AWS Access Key set in the AWS config", function () {
     var config = AWS.config;
     config.credentials.accessKeyId.should.equal(process.env.AWS_ACCESS_KEY_ID);
     config.credentials.secretAccessKey.should.equal(process.env.AWS_SECRET_KEY);
   })
 
-  it('should have the AWS region set to us west 2', function () {
+  it("should have the AWS region set to us west 2", function () {
     AWS.config.region.should.equal(awsRegion);
   })
 
-  it('should send a message to the queue', function (done) {
-
-
+  // This is a test to show an example of AWS.SDK SQS usage, not particularly an
+  // example of the kind of test you"d actually want to have in an application.
+  it("should have an available queue", function (done) {
     var params = {
-      QueueNamePrefix: ''
+      QueueNamePrefix: ""
     };
     sqs.listQueues(params, function (err, data) {
 
-
       if (err) console.log(err, err.stack); // an error occurred
       else {
-        console.log(data);
+        var queueUrls = data.QueueUrls;
+        queueUrls.length.should.be.greaterThan(0);
         done();
-      }// successful response
-
-
+      }
     });
-
-
   })
 
-});
+  // This is a test to show an example of AWS.SDK SQS usage, not particularly an
+  // example of the kind of test you"d actually want to have in an application.
+  it("should post a message the queue", function (done) {
+    var params = {
+      MessageBody: "The Message Body Goes Here",
+      QueueUrl: "https://sqs.us-west-2.amazonaws.com/588271471917/a_sample",
+      //QueueUrl: "https://sqs.us-west-2.amazonaws.com/588271471917/sample",
+      DelaySeconds: 0
+    };
 
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } // an error occurred
+      else {
+        data.MD5OfMessageBody.should.exist;
+        data.MessageId.should.exist;
+        done();
+      }
+      ;
+    });
+  })
+  
+});
